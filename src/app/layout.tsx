1b09c2de05dbc4ad4c02cd929cfb7e647d821433
JD Padrnos <jdpadrnos@gmail.com>
2023-12-03 00:40:01 -0800

    Revert "Back end"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import Providers from '../../components/Providers'
-import Appbar from '../../components/Appbar'
 import { ImageProvider } from '../../components/ImageContext'
 import { IngredientProvider } from '../../components/IngredientContext'
 import { BarcodeProvider } from '../../components/BarcodeContext'
@@ -17,16 +15,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Providers>
-        <Appbar></Appbar>
-        <IngredientProvider>
-          <ImageProvider>
-            <BarcodeProvider>
-              <body className="bg-theme text-color">{children}</body>
-            </BarcodeProvider>
-          </ImageProvider>
-        </IngredientProvider>
-      </Providers>
+      <IngredientProvider>
+        <ImageProvider>
+          <BarcodeProvider>
+            <body className="bg-theme text-color">{children}</body>
+          </BarcodeProvider>
+        </ImageProvider>
+      </IngredientProvider>
     </html>
   )
 }
